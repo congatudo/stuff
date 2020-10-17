d5c2077341b7600cee21573c7b56c3f2956879f6
v99 __ <darkflock@gmail.com>
2020-10-17 12:42:11 +0000

    Upload CecotecIn File

---
+++ other/CecotecIn.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import os.path as path
+from distutils.util import strtobool
+import sys
+import os
+import time
+import binascii
+
+class Module:
+    def __init__(self, incoming=False, verbose=False, options=None):
+        # extract the file name from __file__. __file__ is proxymodules/name.py
+        self.name = path.splitext(path.basename(__file__))[0]
+        self.description = 'Cecotec protocol in module'
+        self.verbose = verbose
+        self.source = None
+        self.destination = None
+        self.incoming = incoming
+        if options is not None:
+            if 'verbose' in options.keys():
+                self.verbose = bool(strtobool(options['verbose']))
+
+    def execute(self, data):
+
+        size = len(data)
+        msg = "Received %d bytes" % size
+        ts = time.time()
+        sc=';'
+        nl = '\n'
+
+        if self.verbose:
+            msg += " from %s:%d" % self.source
+            msg += " for %s:%d" % self.destination
+
+        #print(msg)
+
+        my_data = []
+        my_data.append('in') # DIRECTION
+        my_data.append(str(ts)) # DIRECTION
+        my_data.append(str(int.from_bytes(data[0:4],byteorder=sys.byteorder))) # SIZE
+        my_data.append(str(int.from_bytes(data[4:5],byteorder=sys.byteorder))) # CTYPE
+        my_data.append(str(int.from_bytes(data[5:6],byteorder=sys.byteorder))) # FLOW
+        my_data.append(str(int.from_bytes(data[6:9],byteorder=sys.byteorder))) # CID
+        my_data.append(str(int.from_bytes(data[10:14],byteorder=sys.byteorder))) # DID
+        my_data.append(hex(int.from_bytes(data[14:22],byteorder='little'))) # SEQ
+        strcommand=str(int.from_bytes(data[22:24],byteorder='little'))
+        #command1="grep '\s"+strcommand+"\s' robotfuntions | awk '{print $NF}' | xargs"
+        #stdoutcommand=os.popen(command1 ).read().strip()
+        #my_data.append(stdoutcommand) # NAME
+        my_data.append(strcommand) # COMMAND
+
+        print(f"{sc.join(my_data)}")
+
+        if len(data)>24:
+            #print("protobuf " + str(data[24:]))
+            protobuf_data = str(data[24:].hex())
+            binary_data = binascii.unhexlify(protobuf_data)
+            with open("blob-file-in", "wb") as f: f.write(binary_data)
+            stdout=os.popen('../protobuf-inspector/main.py < blob-file-in').read() 
+            print(stdout)
+
+        return data
+
+    def help(self):
+        h = '\tverbose: override the global verbosity setting'
+        return h
+
+
+if __name__ == '__main__':
+    print('This module is not supposed to be executed alone!')
